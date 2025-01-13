1e142ffb2a36f21ec35ef1a5e95713e771e26fa3
Tatyana Poteshkina <57525148+IsaakWoof@users.noreply.github.com>
2025-01-13 14:55:21 +0500

    dgdg

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', number=0)
 
 @app.route('/assets/<path:filename>')
 def serve_assets(filename):
