3aca22e048b7e55ba1ff5c646f936f8fdda0ffd1
Tatyana Poteshkina <57525148+IsaakWoof@users.noreply.github.com>
2025-01-13 13:58:06 +0500

    shdsad

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/assets/<path:filename>')
+def serve_assets(filename):
+    return send_from_directory('assets', filename)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
