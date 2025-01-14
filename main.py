249ce53fcd783b331ac2b429ce7e8c70851c6e45
Tatyana Poteshkina <57525148+IsaakWoof@users.noreply.github.com>
2025-01-14 14:01:31 +0500

    add counter sync

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, send_from_directory, redirect
+from flask import Flask, render_template, send_from_directory, request, jsonify
 app = Flask(__name__)
 
+num = 0
 
 @app.route('/')
 def index():
@@ -9,6 +10,19 @@ def index():
 @app.route('/assets/<path:filename>')
 def serve_assets(filename):
     return send_from_directory('assets', filename)
+
+@app.route('/increase', methods=["POST"])
+def increase():
+    global num
+    data: dict[str, int] = request.get_json()
+    if not data:
+        return 400
+    n = data.get("number", None)
+    if n == None:
+        return 400
+    num = max(num, n)
+
+    return jsonify({"counter": num}), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
